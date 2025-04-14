78b4170c0877a0f20d0e98f445e78379214c3a35
Prisha <prisha09@cs.washington.edu>
2025-04-13 22:22:01 -0700

    Menu Css

--- src/Menu.js
+++ src/Menu.js
@@ -1,11 +1,17 @@
 import React from "react";
 import menuImage from './pictures/menu.jpg';
+import './Menu.css';
 
 const Menu = () => {
-    return (
-      <section>
-      <img src={menuImage} alt='' width='654' height='925' />
-      </section>
-    );
+  return (
+    <section className="menu-section">
+      <div className="menu-container">
+        <div className="menu-header">
+          <h1 className="menu-title">MENU</h1>
+        </div>
+        <img src={menuImage} alt="Menu" className="menu-image" />
+      </div>
+    </section>
+  );
 };
 export default Menu;
