adf1b1da73ab163dd0f9b253c308353e65e1113e
Swara Ramesh <swaru.ramesh@gmail.com>
2025-04-19 12:02:56 -0700

    Fixed NavBar and FFooter positioning

--- src/YokaTeaApp.js
+++ src/YokaTeaApp.js
@@ -9,13 +9,17 @@ import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 function YokaTeaApp() {
     return (
         <Router>
+            <div className="page-container">
             <Navbar />
-            <Routes>
-                <Route path="/menu" element={<Menu />} />
-                <Route path="/about" element={<AboutUs />} />
-                <Route path="/profile" element={<Profile />} />
-            </Routes>
+            <main className="content-wrap">
+                <Routes>
+                    <Route path="/menu" element={<Menu />} />
+                    <Route path="/about" element={<AboutUs />} />
+                    <Route path="/profile" element={<Profile />} />
+                </Routes>
+            </main>
             <Footer />
+            </div>
         </Router>
     );
 }
