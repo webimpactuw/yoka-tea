f91ce45e8f422b75803e0758e8c73194335b44b7
Caleb Hu <chu552@cs.washington.edu>
2025-04-29 15:11:53 -0700

    About Us content - CSS file to be completed

---
+++ src/AboutUs.js
@@ -0,0 +1,19 @@
+import React from 'react';
+
+function AboutUs() {
+
+    return (
+        <div className="AboutUs">
+            <h1>About Us</h1>
+            <hr />
+            <img src="./pictures/Copy of FU008291.JPG" />
+
+            <h2>More than BOBA.</h2>
+            <p>Welcome to Yoka Tea—where we are more than just boba. We're a haven of unity in a world that often feels divided.<br></br>Explore both classic and unique concoctions on our menu, and dive into a true boba experience that transcends the ordinary. Join us on a journey beyond the cup, where every sip is a celebration of connection and creativity.</p>
+            <h2>Concept Store.</h2>
+            <p>Embark on a fulfilling journey with us in this temporary space. While we prepare for our official storefront, we will humbly share a glimpse of what Yoka has to offer. Join us on weekends, as we eagerly anticipate growing together with you.</p>
+        </div>
+                ;
+            }
+
+export default AboutUs;
