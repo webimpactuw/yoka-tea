6dd6955e194abd9b86d37c979f4ad802b5ea74d9
Caleb Hu <chu552@cs.washington.edu>
2025-04-29 17:29:42 -0700

    About Us with CSS - needs some work

--- src/AboutUs.js
+++ src/AboutUs.js
@@ -1,19 +1,27 @@
 import React from 'react';
+import './AboutUs.css';
+import aboutUsImage from './pictures/Copy of FU008291.JPG';
+
 
 function AboutUs() {
 
     return (
+
         <div className="AboutUs">
-            <h1>About Us</h1>
+
+            <h1 class="title">ABOUT US</h1>
             <hr />
-            <img src="./pictures/Copy of FU008291.JPG" />
-
+            <img src={aboutUsImage} id="about-us-image" alt="About Us" />
             <h2>More than BOBA.</h2>
-            <p>Welcome to Yoka Tea—where we are more than just boba. We're a haven of unity in a world that often feels divided.<br></br>Explore both classic and unique concoctions on our menu, and dive into a true boba experience that transcends the ordinary. Join us on a journey beyond the cup, where every sip is a celebration of connection and creativity.</p>
-            <h2>Concept Store.</h2>
-            <p>Embark on a fulfilling journey with us in this temporary space. While we prepare for our official storefront, we will humbly share a glimpse of what Yoka has to offer. Join us on weekends, as we eagerly anticipate growing together with you.</p>
-        </div>
-                ;
-            }
+            <p>Welcome to Yoka Tea—where we are more than just boba. We're a haven of unity in a world that often feels divided.</p>
+            <p>Embark on a fulfilling journey with us in this temporary space. While we prepare for our official storefront, we will humbly share a glimpse of what Yoka has to offer. </p>
+             <p>Join us on weekends, as we eagerly anticipate growing together with you.</p>
+            <br></br>
+        </div> 
 
-export default AboutUs;
+            );
+                                                                                    
+}
+      
+    export default AboutUs;
+
