acc5ac6745c3fc5183870915e6aab02eb5808c6f
Caleb Hu <chu552@cs.washington.edu>
2025-05-06 15:29:46 -0700

    About Us page finalized

--- src/AboutUs.js
+++ src/AboutUs.js
@@ -7,21 +7,24 @@ function AboutUs() {
 
     return (
 
-        <div className="AboutUs">
+        <div className="flex-container">
 
-            <h1 class="title">ABOUT US</h1>
-            <hr />
+
             <img src={aboutUsImage} id="about-us-image" alt="About Us" />
-            <h2>More than BOBA.</h2>
-            <p>Welcome to Yoka Tea—where we are more than just boba. We're a haven of unity in a world that often feels divided.</p>
-            <p>Embark on a fulfilling journey with us in this temporary space. While we prepare for our official storefront, we will humbly share a glimpse of what Yoka has to offer. </p>
-             <p>Join us on weekends, as we eagerly anticipate growing together with you.</p>
-            <br></br>
-        </div> 
+            <div className="about-us-content">
+                <h1 class="title">ABOUT US</h1>
+                <hr />
+                <h2>More than BOBA.</h2>
+                <p>Welcome to Yoka Tea—where we are more than just boba. We're a haven of unity in a world that often feels divided.</p>
+                <p>Embark on a fulfilling journey with us in this temporary space. While we prepare for our official storefront, we will humbly share a glimpse of what Yoka has to offer. </p>
+                <p>Join us on weekends, as we eagerly anticipate growing together with you.</p>
+                <br></br>
+            </div>
+        </div >
 
-            );
-                                                                                    
+    );
+
 }
-      
-    export default AboutUs;
+
+export default AboutUs;
 
