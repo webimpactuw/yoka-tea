fc12a7beb4578e3a3cfb3bbe4da86a0fa61bf941
Swara Ramesh <swaru.ramesh@gmail.com>
2025-05-06 16:04:17 -0700

    added logo to navbar

--- src/NavBar.js
+++ src/NavBar.js
@@ -1,19 +1,25 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
 import './NavBar.css';
+import Logo from './pictures/Logo.png';
 
 function Navbar() {
     return (
+        <div className="navbar-container">
+        <Link to="/" className="logo-link">
+          <img src={Logo} alt="YOKA Logo" className="logo-image" />
+        </Link>
+
         <nav>
-            <div>
-                <ul id="navbar">
-                    <li><Link to="/menu"> Menu </Link></li>
-                    <li><Link to="/about"> About Us </Link></li>
-                    <li><Link to="/profile"> Profile </Link></li>
-                </ul>
-            </div>
+          <ul id="navbar">
+            <li><Link to="/menu">Menu</Link></li>
+            <li><Link to="/about">About Us</Link></li>
+            <li><Link to="/gallery">Gallery</Link></li>
+            <li><Link to="/profile">Profile</Link></li>
+          </ul>
         </nav>
-    )
+      </div>
+    );
 }
 
 export default Navbar;
