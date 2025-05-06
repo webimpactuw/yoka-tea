e9413e4532bf8fa2c10555e826fd28efcd7e8670
Caleb Hu <chu552@cs.washington.edu>
2025-05-06 15:34:59 -0700

    Bolded Yoka Tea in About Us page

--- src/AboutUs.js
+++ src/AboutUs.js
@@ -15,7 +15,7 @@ function AboutUs() {
                 <h1 class="title">ABOUT US</h1>
                 <hr />
                 <h2>More than BOBA.</h2>
-                <p>Welcome to Yoka Tea—where we are more than just boba. We're a haven of unity in a world that often feels divided.</p>
+                <p>Welcome to <strong>Yoka Tea</strong>—where we are more than just boba. We're a haven of unity in a world that often feels divided.</p>
                 <p>Embark on a fulfilling journey with us in this temporary space. While we prepare for our official storefront, we will humbly share a glimpse of what Yoka has to offer. </p>
                 <p>Join us on weekends, as we eagerly anticipate growing together with you.</p>
                 <br></br>
